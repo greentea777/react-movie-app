0161200c2887a583faa9c7cc0c2d8c67c18dcf19
zy-link <Y_1st.Zenith@icloud.com>
2023-09-10 19:55:51 -0700

    Home links to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Nav from "./Nav";
+import { NavLink } from "react-router-dom";
 import useMediaQuery from "../hooks/useMediaQuery";
 
 const Header = () => {
@@ -6,7 +7,11 @@ const Header = () => {
   return (
     <header className="fixed flex-col top-0 left-0 right-0 flex items-center bg-black text-white sm:flex-row z-50">
       <div className="flex justify-center p-2 sm:pl-5">
-        <div className="w-12 bg-red-700 h-12"></div>
+        <NavLink
+          to="/"
+        >
+          <div className="w-12 bg-red-700 h-12" />
+        </NavLink>
       </div>
       <Nav isBrowser={isBrowser} />
     </header>
