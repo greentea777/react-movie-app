2179d70bd1e2e95490a02ab95747f106cbb58a6e
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-09-10 20:07:25 -0700

    added favourite button animation when favourited

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -1,7 +1,7 @@
 import { IconContext } from "react-icons";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import {
   FavouriteListContext,
   FavouriteDispatchContext,
@@ -10,24 +10,25 @@ import {
 const FavouriteButton = ({ movie }) => {
   const dispatch = useContext(FavouriteDispatchContext);
   const favouriteList = useContext(FavouriteListContext);
+  const isFavourite = favouriteList?.find((item) => item.id === movie?.id);
+  const [isAnimating, setIsAnimating] = useState(false);
+
   return (
     <button
+      className={isAnimating && isFavourite ? "animate-ping-once" : ""}
       onClick={() => {
         dispatch({
           type: "toggle",
           id: movie.id,
           movie: movie,
         });
+        setIsAnimating(true); // 启用动画
       }}
     >
       <IconContext.Provider
-        value={{ size: "1.5em", className: "global-class-name" }}
+        value={{ size: "1.5em", color: "red", className: "global-class-name" }}
       >
-        {favouriteList?.find((item) => item.id === movie?.id) ? (
-          <FaHeart />
-        ) : (
-          <FaRegHeart />
-        )}
+        {isFavourite ? <FaHeart /> : <FaRegHeart />}
       </IconContext.Provider>
     </button>
   );
