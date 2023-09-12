65889e3ec48d70c3eee162fd39069b0e7f53b037
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-09-11 22:22:34 -0700

    switched fav list ContexAPI to Redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppRouter from "./routers/AppRouter";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   </React.StrictMode>
 );
